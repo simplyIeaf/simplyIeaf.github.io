fe91286b375638b7fac5d60026e8aabf2bcfd841
Leaf <itzsimplyleaf345@gmail.com>
2026-07-13 19:10:24 +0200

    Create/update scripts/shop-system-serverscript/raw/shop-system-serverscript.lua

---
+++ scripts/shop-system-serverscript/raw/shop-system-serverscript.lua
@@ -0,0 +1,20 @@
+local Marketplaceservice = game:GetService("MarketplaceService")
+local Players = game:GetService("Players")
+
+ 
+Marketplaceservice.ProcessReceipt = function(receiptInf)
+    if receiptInf.ProductId == 3609650354 then -- replace the id with your developer product id
+        -- replace this with your own logic (this kills everyone in the server)
+        for _, player in ipairs(Players:GetPlayers()) do
+            local character = player.Character 
+            if character then
+                local humanoid = character:FindFirstChildOfClass("Humanoid")
+                if humanoid then
+                    humanoid.Health = 0
+                end
+            end
+        end    
+        return Enum.ProductPurchaseDecision.PurchaseGranted
+    end
+    return Enum.ProductPurchaseDecision.NotProccesedYet
+end
